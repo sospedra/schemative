59603ba1d2095d9d3c677e893ee32011421a2749
sospedra <sospedra.r@gmail.com>
2016-11-24 23:34:02 +0100

    test: Add createSchema suite

--- __tests__/unit.spec.js
+++ __tests__/unit.spec.js
@@ -3,7 +3,7 @@ import { PropTypes } from 'react'
 import * as Schemative from '../src/schemative'
 import * as mocks from './mocks'
 
-describe('Types suite', () => {
+describe('Suite types', () => {
   it('should have exist all the defined types', () => {
     mocks.types.forEach((type) => {
       expect(Schemative[type]).toMatchSnapshot()
@@ -22,5 +22,51 @@ describe('Types suite', () => {
     mocks.functionTypes.forEach(([type, baseType]) => {
       expect(Schemative[type](baseType).propTypes).toEqual(PropTypes[type])
     })
+  })
+})
+
+describe('Suite createSchema', () => {
+  it('should create from a simple non-nested definition', () => {
+    const simple = {
+      name: Schemative.string,
+      year: Schemative.number
+    }
+    expect(Schemative.createSchema(simple)).toMatchSnapshot()
+  })
+
+  it('should create from a complex non-nested definition', () => {
+    const complex = {
+      contacts: Schemative.array,
+      geo: Schemative.object,
+      getName: Schemative.func
+    }
+    expect(Schemative.createSchema(complex)).toMatchSnapshot()
+  })
+
+  it('should create from a simple nested definition', () => {
+    const simpleNested = {
+      name: Schemative.string,
+      contacts: Schemative.arrayOf([
+        Schemative.number,
+        Schemative.number
+      ])
+    }
+    expect(simpleNested).toMatchSnapshot()
+  })
+
+  it('should create from a complex nested definition', () => {
+    const complexNested = {
+      geo: Schemative.objectOf({
+        coord: Schemative.number,
+        country: Schemative.shape({
+          id: Schemative.number,
+          flag: Schemative.arrayOf([
+            Schemative.string,
+            Schemative.string
+          ])
+        })
+      })
+    }
+    expect(complexNested).toMatchSnapshot()
   })
 })
