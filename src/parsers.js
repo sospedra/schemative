830a999e64611d43521fb1c0c340d45a964db4f3
sospedra <sospedra.r@gmail.com>
2016-11-27 01:42:31 +0100

    feat: Add proto mutators on transform
    BREAKING CHANGE: New syntax can cause conflicts with old invokes

--- src/parsers.js
+++ src/parsers.js
@@ -42,10 +42,20 @@ export const filterByKeys = (definition, candidate) => {
   return _.pick(candidate, keys)
 }
 
-export const transform = (definition, candidate, mutators) => Object.assign(
-  filterByKeys(definition, candidate),
-  _.reduce(mutators, (memo, value, key) => Object.assign({
-    [key]: _.isFunction(value) ? value(candidate) : value,
-    memo
-  }), {})
-)
+let protoMutators = {}
+
+export const transform = (definition, candidate, mutators = protoMutators) => {
+  return Object.assign(
+    filterByKeys(definition, candidate),
+    _.reduce(mutators, (memo, value, key) => Object.assign({
+      [key]: _.isFunction(value) ? value(candidate) : value
+    }, memo), {})
+  )
+}
+
+Object.defineProperty(transform, 'mutators', {
+  get: () => protoMutators,
+  set: (mutators) => {
+    protoMutators = mutators
+  }
+})
