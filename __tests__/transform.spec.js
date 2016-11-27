86341bb10069eab5f014200d541e03cedf3226bb
sospedra <sospedra.r@gmail.com>
2016-11-27 14:05:49 +0100

    test: Check transform definitions are well set

--- __tests__/transform.spec.js
+++ __tests__/transform.spec.js
@@ -3,8 +3,9 @@ import { filterByKeys, transform } from '../src/transform'
 import * as mocks from './mocks'
 
 describe('Transform suite', () => {
+  const keys = Object.keys(mocks.defs.simple)
+
   it('should filter by keys two objects', () => {
-    const keys = Object.keys(mocks.defs.simple)
     expect(filterByKeys(keys, mocks.defs.complex)).toEqual(mocks.defs.simple)
   })
 
@@ -17,7 +18,15 @@ describe('Transform suite', () => {
   })
 
   it('should be able to define agent mutators', () => {
-    transform.mutators = { foo: 'bar' }
+    const mutator = { foo: 'bar' }
+    transform.mutators = mutator
+
     expect(transform(mocks.defs.simple, mocks.fulfilled)).toMatchSnapshot()
+    expect(transform.mutators).toEqual(mutator)
+  })
+
+  it('should be able to define keys declaratively', () => {
+    transform.keys = keys
+    expect(transform.keys).toEqual(keys)
   })
 })
