6426f4f6a15947699bb70d9129c0ef917b7743a0
sospedra <sospedra.r@gmail.com>
2016-11-27 01:02:42 +0100

    refactor: Split parsers test out

---
+++ __tests__/parsers.spec.js
@@ -0,0 +1,52 @@
+/* global describe, it, expect */
+import * as parsers from '../src/parsers'
+import * as mocks from './mocks'
+
+describe('Suite parsers', () => {
+  const {
+    recursiveObject,
+    recursiveArray,
+    selectRecursiveStrategy,
+    createPropTypes,
+    createDefault,
+    filterByKeys,
+    transform
+  } = parsers
+
+  it('should create an schema given an object', () => {
+    expect(recursiveObject(mocks.defs.object)).toMatchSnapshot()
+  })
+
+  it('should create an schema given an array', () => {
+    expect(recursiveArray(mocks.defs.array)).toMatchSnapshot()
+  })
+
+  it('should select an strategy for recursivity', () => {
+    expect(selectRecursiveStrategy(mocks.defs.array)).toMatchSnapshot()
+    expect(selectRecursiveStrategy(mocks.defs.object)).toMatchSnapshot()
+  })
+
+  it('should create the schema prop types', () => {
+    expect(createPropTypes(mocks.defs.schemativePropTypes)).toMatchSnapshot()
+  })
+
+  it('should create the schema prop types with isRequired', () => {
+    expect(createPropTypes(mocks.defs.isRequiredPropTypes)).toMatchSnapshot()
+  })
+
+  it('should create the schema default values', () => {
+    expect(createDefault(mocks.defs.schemativePropTypes)).toMatchSnapshot()
+  })
+
+  it('should filter by keys two objects', () => {
+    expect(filterByKeys(mocks.defs.simple, mocks.defs.complex)).toEqual(mocks.defs.simple)
+  })
+
+  it('should transform a definition into a fulfilled object', () => {
+    expect(transform(mocks.defs.simple, mocks.fulfilled)).toEqual(mocks.fulfilled)
+  })
+
+  it('should transform a definition using mutators', () => {
+    expect(transform(mocks.defs.simple, mocks.fulfilled, mocks.mutators)).toMatchSnapshot()
+  })
+})
