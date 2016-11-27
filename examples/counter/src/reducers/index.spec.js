c1cc451d7ffd27345cb88a15a87910d62845aa32
sospedra <sospedra.r@gmail.com>
2016-11-27 14:09:07 +0100

    chore(example): Update tests to match Schemative

--- examples/counter/src/reducers/index.spec.js
+++ examples/counter/src/reducers/index.spec.js
@@ -1,21 +1,26 @@
 import counter from './index'
+import defaultState from '../state'
 
 describe('reducers', () => {
   describe('counter', () => {
+    const incrementedState = Object.assign({
+      value: 1
+    }, defaultState)
+
     it('should provide the initial state', () => {
-      expect(counter(undefined, {})).toBe(0)
+      expect(counter(undefined, {})).toBe(defaultState)
     })
 
     it('should handle INCREMENT action', () => {
-      expect(counter(1, { type: 'INCREMENT' })).toBe(2)
+      expect(counter(incrementedState, { type: 'INCREMENT' })).toMatchSnapshot()
     })
 
     it('should handle DECREMENT action', () => {
-      expect(counter(1, { type: 'DECREMENT' })).toBe(0)
+      expect(counter(incrementedState, { type: 'DECREMENT' })).toMatchSnapshot()
     })
 
     it('should ignore unknown actions', () => {
-      expect(counter(1, { type: 'unknown' })).toBe(1)
+      expect(counter(incrementedState, { type: 'unknown' })).toMatchSnapshot()
     })
   })
 })
