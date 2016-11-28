23804fa4926d92f07da9aea641cff2c8f8be69e3
sospedra <sospedra.r@gmail.com>
2016-11-28 13:50:11 +0100

    fix: Isolate transform methods

--- src/transform.js
+++ src/transform.js
@@ -1,37 +1,29 @@
 import _ from 'lodash'
 
-let protoMutators = {}
-let defsKeys = {}
-
 export const filterByKeys = (defsKeys, candidate) => {
   const keys = _.intersection(defsKeys, _.keys(candidate))
   return _.pick(candidate, keys)
 }
 
 export const createTransform = (definitions) => {
-  transform.keys = _.keys(definitions)
+  const defsKeys = _.keys(definitions)
+  let protoMutators = {}
+
+  const transform = (candidate, mutators = protoMutators) => {
+    return Object.assign(
+      filterByKeys(defsKeys, candidate),
+      _.reduce(mutators, (memo, value, key) => Object.assign({
+        [key]: _.isFunction(value) ? value(candidate, defsKeys) : value
+      }, memo), {})
+    )
+  }
+
+  Object.defineProperty(transform, 'mutators', {
+    get: () => protoMutators,
+    set: (mutators) => {
+      protoMutators = mutators
+    }
+  })
+
   return transform
 }
-
-export const transform = (candidate, mutators = protoMutators) => {
-  return Object.assign(
-    filterByKeys(defsKeys, candidate),
-    _.reduce(mutators, (memo, value, key) => Object.assign({
-      [key]: _.isFunction(value) ? value(candidate, defsKeys) : value
-    }, memo), {})
-  )
-}
-
-Object.defineProperty(transform, 'mutators', {
-  get: () => protoMutators,
-  set: (mutators) => {
-    protoMutators = mutators
-  }
-})
-
-Object.defineProperty(transform, 'keys', {
-  get: () => defsKeys,
-  set: (definitions) => {
-    defsKeys = definitions
-  }
-})
